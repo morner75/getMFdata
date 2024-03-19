dac4be2959589302ecc5ed494a0a7ec6f292782b
Junho Lee <morner75@gmail.com>
2024-03-19 13:36:35 +0900

    add data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' USDKRW exchange rate data
+#'
+#' The average monthly exchange rate US DOLLAR (USD) to SOUTH KOREAN WON (KRW) since Jan. 1990.
+#'
+#'
+#' @format ## `usdkrw`
+#' A data frame with 410 rows and 2 columns:
+#' \describe{
+#'   \item{yearmon}{year.month as a numeric}
+#'   \item{usdkrw3}{exchange rate}
+#' }
+#' @source <https://ecos.bok.or.kr/>
+"usdkrw"
